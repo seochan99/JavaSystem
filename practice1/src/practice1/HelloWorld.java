c5031de5ac8a9ecf4a20bd67d80daac3beef9601
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-09-13 12:01:05 +0900

    [EDU]2주차-1

--- practice1/src/practice1/HelloWorld.java
+++ practice1/src/practice1/HelloWorld.java
@@ -1,8 +1,22 @@
 package practice1;
 
 public class HelloWorld {
+	public static int sum(int n,int m)
+	{
+		return n+m;
+	}
+	
 	public static void main(String[] args)
 	{
-		System.out.print("hello World");
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i,10);
+		a = '?';
+		System.out.println(a);
+		
+		System.out.println("hello World");
+		System.out.println(s);
 	}
 }
