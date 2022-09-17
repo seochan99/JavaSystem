c55a50ca3307fb34a5fdce53740c74a810f1fa78
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-09-17 12:36:08 +0900

    3-3

---
+++ switchCasePractice/src/switchCasePractice/rockSisPaper.java
@@ -0,0 +1,30 @@
+package switchCasePractice;
+import java.util.Scanner;
+
+
+public class rockSisPaper {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String choice = scanner.next();
+		
+		switch (choice) {
+			case "가위" :
+				System.out.println(1);
+				break;
+			case "바위" :
+				System.out.println(2);
+				break;
+			case "보" :
+				System.out.println(3);
+				break;
+			default:
+				System.out.println(0);
+			}
+		scanner.close();
+
+
+	}
+
+}
