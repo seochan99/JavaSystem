7daaa135417007c582b2084f3cccf0cea55203f3
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-09-22 16:23:35 +0900

    Update newScanner2.java

--- matrix/src/matrix/newScanner2.java
+++ matrix/src/matrix/newScanner2.java
@@ -7,13 +7,15 @@ public class newScanner2 {
 	public static void main(String[] args) {
 		System.out.println("이름, 나이, 몸무게 입력 ");
 		Scanner sc = new Scanner(System.in);
-		
+	try {
 		String name = sc.next();
 		int age = sc.nextInt();
 		double weight = sc.nextDouble();
-		
-		
 		System.out.println("Message\n"+name+"\n"+age+"\n"+weight);
+	}
+	catch(java.util.InputMismatchException e) {
+		System.out.println("NO DATA");
+	}
 		
 		
 
