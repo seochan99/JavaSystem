223367da7097f4b2dde413ae82ad1687a1e3b70f
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-09-22 16:03:00 +0900

    systemInRead

---
+++ matrix/src/matrix/matrixTool.java
@@ -0,0 +1,7 @@
+package matrix;
+
+public class matrixTool {
+	  private int row;    // 행
+	private int col;    // 열
+
+}
---
+++ matrix/src/matrix/system_in.java
@@ -0,0 +1,33 @@
+package matrix;
+
+import java.io.IOException;
+
+public class system_in {
+
+	public static void main(String[] args) {
+		byte[] messageFromKeyboard = new byte[256];
+		
+		System.out.println("Type a Message : ");
+		//키보드 입력을 바로 저장하니깐// raw 데이터처럼 써먹기 가능// 이렇게 입력받아야하는곳에서는 scanner보다는 이 방식이더좋음 
+		
+		// 
+		try {
+			System.in.read(messageFromKeyboard);	
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		
+		for(int i=0;i<messageFromKeyboard.length;i++)
+		{
+			
+			System.out.println(messageFromKeyboard[i]);
+			System.out.println(new String(messageFromKeyboard));
+			
+		}
+		
+
+	}
+
+}
