a8abc6a74d9ae78ac5541db5583eb4d98de700d7
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-10-30 03:50:12 +0900

    7주차 1번

---
+++ example_week8_1/src/example_week8_1/week8_1.java
@@ -0,0 +1,76 @@
+package example_week8_1;
+
+// ArrayList배열 이용 
+import java.util.ArrayList;
+
+// 입력을 위한 scanner 
+import java.util.Scanner;
+
+//정렬을 위한 Collections
+import java.util.Collections;
+
+// error
+import java.util.InputMismatchException;
+
+public class week8_1 {
+
+	public static void main(String[] args) {
+
+		
+				// Scanner
+				Scanner sc = new Scanner(System.in);
+				
+				// ArrayList 선언
+				// list : 초기에 입력된 값 저장하는 배열 
+				ArrayList<Integer> list = new ArrayList<>();
+				
+				// 데이터 변수  
+				int data;
+				
+				System.out.print("Input : ");
+				
+					// 배열 입력 받기 반복문, 10개입력받음.
+					for(int i=0;i<10;i++)
+					{
+						try {
+						// data 변수에 정수 입력받고 리스트에 넣기 
+						data = sc.nextInt();
+						list.add(data);	
+						}
+						// 숫자 미입력시  InputMismatchException 에러, 재입력 요
+						catch(InputMismatchException e) {
+							System.out.println("정수가 아닙니다. 다시 입력해주세요.");
+							i--; // i빼고for문으로 돌아가 다시 입력 받기 
+							sc.next(); // 무한루프 방지를 위해 입력 스트림 토큰 비우기
+							continue;
+						}	
+					}
+					
+					// 오름차순 정렬(Collections.sort사용) 
+					Collections.sort(list);
+					
+					
+					//출력
+					System.out.print("Output :");
+					
+					// get(i),for문으로 ArrayList 요소별로 가져오
+					for(int i=0;i<list.size();i++)
+					{
+						// 가져 온 요소가 3으로나눈 나머지가0이면 해당 값 출력하기 
+						if(list.get(i) % 3 == 0)
+							System.out.print(" "+list.get(i));
+						// 그 외의 값들은 출력 x 
+						else
+							continue;
+					}
+					
+					// 스캐너 닫기 
+					sc.close();
+				}
+			
+				
+
+				
+
+	}
+
--- fileIO/src/fileIO/input.java
+++ fileIO/src/fileIO/input.java
@@ -1,7 +1,68 @@
 package fileIO;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+
 public class input {
 	public static void main(String[] args) {
+		// Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		
+		// ArrayList 선언
+		// beforeList : 초기에 입력된 값 저장하는 배열 
+		// afterList : beforeList에서 3의 배수인 값을 저장할 열 
+		ArrayList<Integer> beforeList = new ArrayList<>();
+		ArrayList<Integer> afterList = new ArrayList<>();
+		
+		// 데이터 변수  
+		int data;
+		
+		System.out.print("Input : ");
+		try {
+			// 배열 입력 받기 반복문 
+			for(int i=0;i<10;i++)
+			{
+				// data 변수에 정수 입력받기 
+				
+				data = sc.nextInt();
+				
+				//  3으로 나눠떨어지는 수만 리스트에 넣음
+				if( data%3 == 0) {
+					// data 변수 리스트에 넣어주기
+					beforeList.add(data);	
+				}
+				// 3으로 나눠지지 않으면 리스트에 안넣음 
+				else {
+					continue;
+				}
+				
+			}
+		}catch(InputMismatchException e) {
+			
+		}
+	
+		
+		//정수 입력안됐을때 예외처리
+		
+		// 배열보다 더 많은 값 입력됐을때 예외처리 
+
+		
+		
+		// 오름차순 정렬(Collections.sort사용) 
+		Collections.sort(beforeList);
+		
+		
+		
+		//출력
+		System.out.print("Output :");
+		
+		// get(i),for문으로 ArrayList 요소별로 가져오
+		for(int i=0;i<beforeList.size();i++)
+		{
+			System.out.print(" "+beforeList.get(i));
+		}
 		
 	}
 
