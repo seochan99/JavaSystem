488a1ff290a75ecee2294717d776721b7f4466ae
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-11-05 21:05:52 +0900

    week9 : start

---
+++ example_week9_1/src/Student.java
@@ -0,0 +1,5 @@
+// input.txt 파일 가져와서 여기 저장 
+public class Student {
+	
+
+}
---
+++ example_week9_1/src/StudentManager.java
@@ -0,0 +1,14 @@
+
+public class StudentManager {
+
+	public static void main(String[] args) {
+		// input.txt 파일 가져오기 
+		
+		// Student 클래스 인스턴스 생성하여 각 정보 저장 
+		
+		// 이를 output.txt에 저장 
+		
+
+	}
+
+}
