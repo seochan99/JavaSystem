60a5b47ef43643586a5f3974bb1611366dd5f3d6
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-11-07 02:53:53 +0900

    9주차 과제 1차 완료

--- example_week9_1/src/Student.java
+++ example_week9_1/src/Student.java
@@ -1,5 +1,85 @@
 // input.txt 파일 가져와서 여기 저장 
 public class Student {
+	// 이름 학번 중간 기말 총점 성적
+	// name, code, mid, kimal, total, rank
+	String name; // 이름 
+	int code; // 학번 
+	int mid; // 중간 
+	int kimal; // 기말 
+	int total; // 총점 
+	char rank; //성적  
+	
+	
+	// 성적은 필드 가공이 필요함 => getter 
+	// why?: 총점을 기준으로 랭크를 매겨야하므로
+	// 이름, 학번, 중간, 기말은 줌 
+	// setter으로 받음
+	
+	Student (){
+		
+	}
+	// 생성자
+	Student (String name, int code, int mid, int kimal){
+		this.name = name;
+		this.code = code;
+		this.mid = mid;
+		this.kimal = kimal;
+	}
+	
+	// 이름 setter
+	public void setName(String name) {
+		this.name = name; 
+	}
+	
+	// 학번 setter 
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+	// mid setter, 50점 위의 값이 입력되면...?
+	public void setMid(int mid) {
+		
+		// 0보다 작거나 50보다 작은 값 입력되면 -1로 저장 
+		if(mid>50 || mid<0) {
+			System.out.print(this.name + "학생의 중간 성적이 잘못 입력되어 -1 처리 합니다.");
+			this.mid = -1;
+		}
+		// 그 외에는 mid 저장 
+		else {
+			this.mid = mid;
+		}
+	}
+	
+	// kimal setter, 50점 위의 값이 입력되면...?
+	public void setKimal(int kimal) {
+		
+		// 0보다 작거나 50보다 작은 값 입력되면 -1로 저장 
+		if(kimal>50 || kimal<0) {
+			this.kimal= -1; 
+			System.out.print(this.name + "학생의 기말 성적이 잘못 입력되어 -1 처리 합니다.");
+		}
+		// 그 외에는 mid 저장 
+		else {
+			this.kimal = kimal;
+		}
+	}
+	
+	// 그런데.. 총점이 100점이 넘어가면 특수 값 저장해서 오류임을 알려주기
+	public void setTotal() {
+		total = kimal + mid;
+	}
+	
+
+	public void getResult() {
+		if(total>=70) {
+			rank = 'A';
+		}else if(total<70 && total>=50) {
+			rank = 'B';
+		}else {
+			rank = 'C';
+		}
+	}
+		
 	
 
 }
--- example_week9_1/src/StudentManager.java
+++ example_week9_1/src/StudentManager.java
@@ -1,13 +1,120 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class StudentManager {
 
 	public static void main(String[] args) {
-		// input.txt 파일 가져오기 
-		
-		// Student 클래스 인스턴스 생성하여 각 정보 저장 
-		
-		// 이를 output.txt에 저장 
-		
+		       
+		// Student Class => getter, setter method 포함
+		// Student Class Collection or Map save 가능 
+	    File outFile = new File("/Users/seochan/Documents/Github/JavaSystem/example_week9_1/src/output.txt");
+
+		// 결과 : 배열 or ArrayList에 저장 -> output.txt 출력 
+			try {
+				// 스캐너 클래스를 이용, 파일의 텍스트를 delimiter를 이용하여 자르기 위해 
+				// input.txt를 입력받음 
+				Scanner sc = new Scanner(new File("/Users/seochan/Documents/Github/JavaSystem/example_week9_1/src/input.txt"));
+				
+				// 결과 저장 ArrayList : studentArray 
+				ArrayList<Student> studentArray = new ArrayList<Student>();
+
+				// 첫줄 "이름 학번 중간 기말 총점 성적" 저장  
+				String firstLine = sc.nextLine();
+				
+				
+				
+				String name ="";
+				int code = 0, mid =0, kimal=0;
+				
+				// input.txt입력받은 값을 Student 클래스타입을 가진 배열 studentArray 요소에 저장
+				// 즉,학생 한명마다 하나의배열 요소 차지함
+				// ex..)
+				// 0번째 학생 : studentArray의 0번째
+				// 0번째 학생의 이름 : studentArray의 0번째.name
+				// 와 같이 저장하기 위해 아래 반문 실행 
+				
+				// 학생 정보체크를 위한 i 선언
+				// hasNext, 반복문 
+				int i =0;
+				
+				// hasNext : 
+		        while (sc.hasNext()) {
+		        	
+		        	// next() method 
+		        	// => default delimiter ' '
+		        	// => 공백으로 쪼갬 
+		        	// =>단어 단위로 읽어 올 수 있
+		        	if(i==0) { // i==0ㅣ면 이면 name
+		        		name = sc.next(); 
+		        	}else if(i==1) { // i==1이면 학번 
+		        		String stringCode = sc.next();
+		        		// string to integer 
+		        		code = Integer.valueOf(stringCode).intValue(); 
+		        	}else if(i==2) {
+		        		//2이면 중간고사 
+		        		String stringMid = sc.next();
+		        		// string to integer 
+		        		mid = Integer.valueOf(stringMid).intValue();
+		        		
+		        	}else if(i==3) {
+		        		//3이면 기고사 
+		        		String stringKimal = sc.next();
+		        		// string to integer 
+		        		kimal = Integer.valueOf(stringKimal).intValue();
+		        	}
+		        	
+		        	// i올려주기 
+		        	i++;
+		        	
+		        	// i가 4라면,정보 모두 얻음, 객체 생성 가능 
+		        	if(i==4) {
+		        		// 객체 생성 
+		        		studentArray.add(new Student(name,code,mid,kimal));
+		        		i=0; // i초기화  
+		        	}
+		        }
+		        
+		        //파일 작성,FileWriter가 outFile가져옴  
+		        FileWriter fw = new FileWriter(outFile);
+		        
+		        // 첫줄 출력 후 개행 
+		        fw.write(firstLine+"\n");
+		        
+		        for(int k=0;k<studentArray.size();k++)
+		        {
+		        	// 객체 total set하기  
+		        	studentArray.get(k).setTotal();
+		        	
+		        	// 객체 rank 가져오기 
+		        	studentArray.get(k).getResult();
+		        	
+		        	//k번째 객체 Name,code,mid,kimal을 가져와서 파일에 작성하기 
+		        	fw.write(studentArray.get(k).name+" ");
+		        	fw.write(studentArray.get(k).code+" ");
+		        	fw.write(studentArray.get(k).mid+" ");
+		        	fw.write(studentArray.get(k).kimal+" ");
+		        	fw.write(studentArray.get(k).total+" ");
+		        	fw.write(studentArray.get(k).rank+" "+"\n");
+		        	
+		        	// 파일 생성완료 여를 알기 위한 코드 
+		        	System.out.println(k+" 파일 생성완");
+		        }
+		        // FileWriter 닫아주기 
+		       fw.close();
+		        
+			}// 파일 못찾을 시 예외처리 
+			catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} // 파일 생성 예외처리  
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
 	}
 
