e6caddce9f8fdb928ee809a237b5593de937ecea
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-11-17 17:00:18 +0900

    feat : OKButton

--- practice_week12/src/practice_week12/ContentPaneEx.java
+++ practice_week12/src/practice_week12/ContentPaneEx.java
@@ -7,7 +7,9 @@ import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 // JFrame을 상속 받아야함 
+
 public class ContentPaneEx extends JFrame{
+
 	JButton OKButton;
 	JButton IgnoreButton;
 	JLabel MessageLabel;
@@ -37,18 +39,86 @@ public class ContentPaneEx extends JFrame{
 		
 		
 		// 그리기 
-		OKButton.addActionListener(new OKButtonActionListener(MessageLabel));
+//		OKButton.addActionListener(new OKButtonActionListener(MessageLabel));
+		OKButton.addActionListener(new OKButtonActionListener_InnerClass());
+		IgnoreButton.addActionListener(new IgnoreButtonActionListener_InnerClass());
+		
+		
+		// 이내용으ㅡㄹ 람다식으로!? 
+		OKButton.addActionListener(new ActionListenr() {
+			@override
+			public void actionPerformed(ActionEvent e) {
+				JButton b = (JButton)e.getSource();
+				if(b.getText() == "Yes") {
+					contentPane.setBackground(Color.cyan);
+				}else {
+					contentPane.setBackground(Color.GREEN);
+				}
+			}
+		});
+		
+		IgnoreButton.addActionListener(event->{
+			JButton bb = (JButton)event.getSource();
+			if(bb.getText()=="Ignore") {
+				OKButton.get
+			}
+		});
+		
+		
 		setSize(300, 150);
 		setVisible(true);
 		
+		// 외부 클래스 => 내부클래스로 
+		// 내부클래스
+		
 		
 	}
+	//#2 : OKButtonActionListener를 inner classs로 
+	class OKButtonActionListener_InnerClass implements java.awt.event.ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// 	// ok누를때마다 바뀌
+			JButton b = (JButton)e.getSource();
+			if(b.getText() == "OK") {
+				b.setText("YES");
+				MessageLabel.setText("OK to Yes");
+			}
+			else {
+				b.setText("OK");
+				MessageLabel.setText("Yes to Ok");
+			}
+			
+		}
+	}
+		
+		class IgnoreButtonActionListener_InnerClass implements java.awt.event.ActionListener{
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// 	// ok누를때마다 바뀌
+				JButton b = (JButton)e.getSource();
+				if(b.getText() == "Ignore") {
+					b.setText("Nope");
+					MessageLabel.setText("Nope to Ignore");
+				}
+				else {
+					b.setText("Ignore");
+					MessageLabel.setText("Ignore to Nope");
+				}
+				
+			}
+		
+	}
+	
 	public static void main(String[] args) {
 		new ContentPaneEx();
 	}
 	
 
 }
+
+// 외부클래스 ㅐ용을 람다식으로 
 class OKButtonActionListener implements java.awt.event.ActionListener{
 	JLabel msgLabel;
 	
