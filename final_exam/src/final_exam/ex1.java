1381d6d058214ca3dc7c106fc3a3380ef80676d3
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-12-03 23:08:53 +0900

    Update ex1.java

--- final_exam/src/final_exam/ex1.java
+++ final_exam/src/final_exam/ex1.java
@@ -1,10 +1,27 @@
 package final_exam;
 
+class GenericPrintHere04<Q>{
+	public void PrintHere(Q vQ) {
+		if(vQ instanceof Integer) {
+			Integer val = (Integer)vQ;
+			System.out.println(val);
+		}
+		if(vQ instanceof Double) {
+			Double val = (Double)vQ;
+			System.out.println("double Value : " + val);
+		}
+	}
+	
+}
 public class ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GenericPrintHere04<Integer> qqq = new GenericPrintHere04<>();
+		qqq.PrintHere(8811);
+		GenericPrintHere04<Double> aaa = new GenericPrintHere04<>();
+		aaa.PrintHere(7477.5);
+		
 	}
-
+			
+			
 }
