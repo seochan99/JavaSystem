a1d92866bbfb6c3465386464363be53db95fa9aa
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-02-18 11:57:14 +0900

    feat : spring setting

---
+++ techit/strpingboot-tutorial/src/main/java/com/example/strpingboottutorial/StrpingbootTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.strpingboottutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrpingbootTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrpingbootTutorialApplication.class, args);
+    }
+
+}
